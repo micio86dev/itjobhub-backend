a26a9e71ed401000fb4c9053509ea5a7f8993321
Micio Dev <micio86dev@gmail.com>
2026-02-08 12:49:03 +0000

    fix: use pino-pretty only in development

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,15 +1,19 @@
 import pino from 'pino';
 
+const isDev = process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'staging';
+
 const logger = pino({
     level: process.env.LOG_LEVEL || 'info',
-    transport: {
-        target: 'pino-pretty',
-        options: {
-            colorize: true,
-            ignore: 'pid,hostname',
-            translateTime: 'SYS:standard',
+    ...(isDev && {
+        transport: {
+            target: 'pino-pretty',
+            options: {
+                colorize: true,
+                ignore: 'pid,hostname',
+                translateTime: 'SYS:standard',
+            },
         },
-    },
+    }),
 });
 
 export default logger;
